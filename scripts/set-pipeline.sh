adbc614084022c760b38b5968fe124f6b7566f61
Eno Compton <ecompton@pivotal.io>
2017-02-17 10:28:01 -0700

    Create release pipeline
    Loggregator is for tests only.
    
    Signed-off-by: JT Archie <jtarchie@gmail.com>

--- scripts/set-pipeline.sh
+++ scripts/set-pipeline.sh
@@ -7,8 +7,15 @@ if [ -z "$pipeline_name" ]; then
     exit 1
 fi
 
-fly -t loggregator set-pipeline -p "$pipeline_name" \
-    -c pipelines/"$pipeline_name".yml \
-    -l ~/workspace/loggregator-credentials/shared-secrets.yml \
-    -l ~/workspace/loggregator-credentials/$pipeline_name.yml \
-    -l ~/workspace/loggregator-ci/scripts.yml
+if [ ! -e ~/workspace/loggregator-credentials/$pipeline_name.yml ]; then
+    fly -t loggregator set-pipeline -p "$pipeline_name" \
+        -c pipelines/"$pipeline_name".yml \
+        -l ~/workspace/loggregator-credentials/shared-secrets.yml \
+        -l ~/workspace/loggregator-ci/scripts.yml
+else
+    fly -t loggregator set-pipeline -p "$pipeline_name" \
+        -c pipelines/"$pipeline_name".yml \
+        -l ~/workspace/loggregator-credentials/shared-secrets.yml \
+        -l ~/workspace/loggregator-credentials/$pipeline_name.yml \
+        -l ~/workspace/loggregator-ci/scripts.yml
+fi
