751067608dfc101a8d09fd509df5c0c1fc1d4e7c
Mark Kollasch <mkollasch@pivotal.io>
2017-06-05 14:58:17 -0600

    Create example application that continuously emits log messages.
    [#146550587]
    
    Signed-off-by: Brady Love <blove@pivotal.io>

---
+++ tools/log_emitter/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var logMessage string
+
+func main() {
+	logsPerSecond := flag.Uint("logs-per-second", 1000, "Log messages to emit per second. Default: 1000")
+	logSize := flag.Uint("log-bytes", 1000, "Length of log messages in bytes. Default: 1000")
+	flag.Parse()
+
+	for i := uint(0); i < *logSize; i++ {
+		logMessage += "!"
+	}
+
+	go func() {
+		for range time.Tick(time.Second) {
+			fmt.Printf("----------------\n")
+		}
+	}()
+
+	interval := time.Second / time.Duration(*logsPerSecond)
+	for {
+		startTime := time.Now()
+		emitLog()
+		timeToSleep := interval - time.Since(startTime)
+		time.Sleep(timeToSleep)
+	}
+}
+
+func emitLog() {
+	fmt.Printf("%s\n", logMessage)
+}
