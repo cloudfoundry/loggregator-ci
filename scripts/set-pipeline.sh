c8696a34149b51da78f6a232657ba08932214c29
Jason Keene <jkeene@pivotal.io>
2017-07-05 10:43:21 -0600

    Update set-pipeline script to set both loggregator and lakitu
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- scripts/set-pipeline.sh
+++ scripts/set-pipeline.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
-pipeline_name=$1
+set -e
 
-if [ -z "$pipeline_name" ]; then
-    echo "usage: $0 <pipeline_name> | all"
-    exit 1
-fi
+function set_globals {
+    env=$1
+    pipeline=$2
+}
 
-function set_pipeline {
-    echo setting pipeline for "$1"
-    fly -t loggregator set-pipeline -p "$1" \
-        -c pipelines/"$1".yml \
+function validate {
+    echo $env env
+    echo $pipeline pipeline
+    if [ -z "$env" ] || [ -z "$pipeline" ]; then
+        echo "usage: $0 <environment> <pipeline | all>"
+        exit 1
+    fi
+}
+
+function loggregator_set_pipeline {
+    local pipeline_name="$1"
+    echo setting pipeline for "$pipeline_name"
+    fly -t loggregator set-pipeline -p "$pipeline_name" \
+        -c "pipelines/loggregator/$pipeline_name.yml" \
         -l ~/workspace/loggregator-credentials/shared-secrets.yml \
         -l ~/workspace/loggregator-ci/scripts.yml
 }
 
-if [ "$pipeline_name" = all ]; then
-    for pipeline_file in $(ls pipelines); do
-        set_pipeline "${pipeline_file%.yml}"
-    done
-else
-    set_pipeline "$pipeline_name"
-fi
+function lakitu_set_pipeline {
+    local pipeline_name="$1"
+    echo setting pipeline for "$pipeline_name"
+    fly -t lakitu set-pipeline -p "$pipeline_name" \
+        -c "pipelines/lakitu/$pipeline_name.yml" \
+        -l ~/workspace/secrets-prod/ci/ci_bosh_secrets.yml \
+        -l ~/workspace/secrets-prod/ci/ci_github_resources.yml
+}
+
+function sync_fly {
+    fly -t "$env" sync
+}
+
+function set_pipelines {
+    if [ "$pipeline" = all ]; then
+        for pipeline_file in $(ls "pipelines/$env"); do
+            "${env}_set_pipeline" "${pipeline_file%.yml}"
+        done
+        exit 0
+    fi
+
+    "${env}_set_pipeline" "$pipeline"
+}
+
+function main {
+    set_globals $1 $2
+    validate
+    sync_fly
+    set_pipelines
+}
+main $1 $2
