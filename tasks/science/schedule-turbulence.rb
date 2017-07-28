c194c9fe8379ebbbaabaffa52a43386d15dd3142
Brady Love <blove@pivotal.io>
2017-07-28 09:33:06 -0600

    Move schedule turbulence script to shared task file
    [#149093601]
    
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- tasks/science/schedule-turbulence.rb
+++ tasks/science/schedule-turbulence.rb
@@ -3,115 +3,120 @@
 require 'net/http'
 require 'json'
 require 'openssl'
-require 'pry'
 
 class MissingRequiredEnvironmentVariable < StandardError; end
 
+class Config
+  attr_accessor :vars_file_path, :base_url, :network_timeout, :network_delay,
+    :network_loss, :network_schedule, :username, :password
+
+  def initialize
+    self.vars_file_path = load_or_raise('VARS_FILE_PATH')
+    self.base_url = load_or_raise('TURBULENCE_API_URL')
+
+    self.network_timeout = load_or_default('TURBULENCE_NETWORK_TIMEOUT', '60m')
+    self.network_delay = load_or_default('TURBULENCE_NETWORK_DELAY', '10ms')
+    self.network_loss = load_or_default('TURBULENCE_NETWORK_LOSS', '5%')
+    self.network_schedule = load_or_default('TURBULENCE_NETWORK_SCHEDULE', '@daily')
+
+    self.username = 'turbulence'
+    self.password = `bosh int vars-store/#{vars_file_path} --path=/turbulence_api_password`.chomp
+  end
+
+  private
+
+  def load_or_raise(name)
+    value = ENV[name]
+    if value.nil?
+      raise MissingRequiredEnvironmentVariable.new(name)
+    end
+
+    value
+  end
+
+  def load_or_default(name, default)
+    ENV[name].nil? ? default : ENV[name]
+  end
+end
+
 class TurbulenceClient
-    def initialize(username, password)
-        self.username = username
-        self.password = password
+  def initialize(config)
+    self.username = config.username
+    self.password = config.password
+    self.base_url = config.base_url
+  end
+
+  def scheduled_incidents
+    resp = do_request(
+      Net::HTTP::Get.new(URI("#{base_url}/api/v1/scheduled_incidents"))
+    )
+    # TODO: Check response status code
+    JSON.parse(resp.body)
+  end
+
+  def delete_scheduled_incidents(incidents)
+    incidents.each do |incidents|
+      uri = URI("#{base_url}/api/v1/scheduled_incidents/#{incidents['ID']}")
+      do_request(Net::HTTP::Delete.new(uri))
+      # TODO: Check response status codes
     end
+  end
 
-    def do_request(request)
-        request.add_field('Accept', 'application/json')
-        request.basic_auth(username, password)
+  def create_scheduled_incident(incident)
+    request_body = JSON.pretty_generate(incident)
+    uri = URI("#{base_url}/api/v1/scheduled_incidents")
+    req = Net::HTTP::Post.new(uri)
+    req.body = request_body
+    req.content_type = 'application/json'
+    resp = do_request(req)
 
-        Net::HTTP.start(
-            request.uri.hostname,
-            request.uri.port,
-            use_ssl: true,
-            verify_mode: OpenSSL::SSL::VERIFY_NONE
-        ) do |http|
-            http.request(request)
-        end
+    if !resp.kind_of?(Net::HTTPSuccess)
+      puts("Failed to create control network scheduled incident: #{resp.inspect}")
+      puts(resp.body)
+      raise
     end
+  end
 
-    private
+  private
 
-    attr_accessor :username, :password
+  attr_accessor :username, :password, :base_url
+
+  def do_request(request)
+    request.add_field('Accept', 'application/json')
+    request.basic_auth(username, password)
+
+    Net::HTTP.start(
+      request.uri.hostname,
+      request.uri.port,
+      use_ssl: true,
+      verify_mode: OpenSSL::SSL::VERIFY_NONE
+    ) do |http|
+      http.request(request)
+    end
+  end
 end
 
-vars_file_path = ENV['VARS_FILE_PATH']
-if vars_file_path.nil?
-    raise MissingRequiredEnvironmentVariable.new('VARS_FILE_PATH')
-end
-
-base_url = ENV['TURBULENCE_API_URL']
-if base_url.nil?
-    raise MissingRequiredEnvironmentVariable.new('TURBULENCE_API_URL')
-end
-
-network_timeout = ENV['TURBULENCE_NETWORK_TIMEOUT']
-if network_timeout.nil?
-   network_timeout = '10m'
-end
-
-network_delay = ENV['TURBULENCE_NETWORK_DELAY']
-if network_delay.nil?
-   network_delay = '10ms'
-end
-
-network_loss = ENV['TURBULENCE_NETWORK_LOSS']
-if network_loss.nil?
-   network_loss = '5%'
-end
-
-network_schedule = ENV['TURBULENCE_NETWORK_SCHEDULE']
-if network_schedule.nil?
-   network_schedule = '@daily'
-end
-
-username = 'turbulence'
-password = `bosh int #{vars_file_path} --path=/turbulence_api_password`.chomp
-
-client = TurbulenceClient.new(username, password)
+puts("Loading config")
+config = Config.new
+client = TurbulenceClient.new(config)
 
 puts('Getting all scheduled incidents')
-resp = client.do_request(
-    Net::HTTP::Get.new(URI("#{base_url}/api/v1/scheduled_incidents"))
-)
-response = JSON.parse(resp.body)
+scheduled_incidents = client.scheduled_incidents
 
-# TODO: Check response status code
+puts("Deleting #{scheduled_incidents.length} scheduled incidents")
+client.delete_scheduled_incidents(scheduled_incidents)
 
-
-puts("Deleting #{response.length} scheduled incidents")
-response.each do |schedule|
-    uri = URI("#{base_url}/api/v1/scheduled_incidents/#{schedule['ID']}")
-    client.do_request(
-        Net::HTTP::Delete.new(uri)
-    )
-    # TODO: Check response status codes
-end
-
-request_body = JSON.pretty_generate({
-    "Schedule" => network_schedule,
-    "Incident" => {
-        "Tasks" => [{
-            "Type" => "control-net",
-            "Timeout" => network_timeout,
-            "Delay" => network_delay,
-            "Loss" => network_loss,
-        }],
-    }
+puts("Creating control network scheduled incident")
+client.create_scheduled_incident({
+  "Schedule" => config.network_schedule,
+  "Incident" => {
+    "Tasks" => [{
+      "Type" => "control-net",
+      "Timeout" => config.network_timeout,
+      "Delay" => config.network_delay,
+      "Loss" => config.network_loss,
+    }],
+  }
 })
 
-puts('Creating control network scheduled incident with:')
-puts(request_body)
-
-uri = URI("#{base_url}/api/v1/scheduled_incidents")
-req = Net::HTTP::Post.new(uri)
-req.body = request_body
-req.content_type = 'application/json'
-resp = client.do_request(req)
-
-if !resp.kind_of?(Net::HTTPSuccess)
-    puts("Failed to create control network scheduled incident: #{resp.inspect}")
-    puts(resp.body)
-    raise
-end
-
-# TODO: Check response status code
-puts('Created control network scheduled incident')
 puts('Done.')
