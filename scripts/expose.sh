908768793b496f33b8a469f7c0d8ace1bee7b46f
Andrew Poydence <apoydence@gmail.com>
2017-08-08 20:32:53 -0600

    add submodules pipeline to expose script

--- scripts/expose.sh
+++ scripts/expose.sh
@@ -5,3 +5,4 @@ fly --target=loggregator expose-pipeline --pipeline=go-loggregator
 fly --target=loggregator expose-pipeline --pipeline=statsd-injector
 fly --target=loggregator expose-pipeline --pipeline=scalable-syslog
 fly --target=loggregator expose-pipeline --pipeline=bosh-hm-forwarder
+fly --target=loggregator expose-pipeline --pipeline=submodules
