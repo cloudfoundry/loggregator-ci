99ab55c70b0032884a7750ba193fa35d637cf690
Eno Compton <ecompton@pivotal.io>
2017-08-31 15:24:25 -0600

    Change scalable syslog to cf syslog drain
    Signed-off-by: Brady Love <blove@pivotal.io>

--- scripts/expose.sh
+++ scripts/expose.sh
@@ -3,6 +3,6 @@
 fly --target=loggregator expose-pipeline --pipeline=loggregator
 fly --target=loggregator expose-pipeline --pipeline=go-loggregator
 fly --target=loggregator expose-pipeline --pipeline=statsd-injector
-fly --target=loggregator expose-pipeline --pipeline=scalable-syslog
+fly --target=loggregator expose-pipeline --pipeline=cf-syslog-drain
 fly --target=loggregator expose-pipeline --pipeline=bosh-hm-forwarder
 fly --target=loggregator expose-pipeline --pipeline=submodules
