21855c9daac9b4e7600484fde9ffbce32418a336
Eno Compton <ecompton@pivotal.io>
2017-10-19 16:07:20 -0600

    Remove superfluous subdirectory

--- scripts/set-pipeline.sh
+++ scripts/set-pipeline.sh
@@ -1,54 +1,43 @@
 #!/bin/bash
 
 function set_globals {
-    env=$1
-    pipeline=$2
+    pipeline=$1
 }
 
 function validate {
-    echo $env env
     echo $pipeline pipeline
-    if [ -z "$env" ] || [ -z "$pipeline" ]; then
-        echo "usage: $0 <environment> <pipeline | all>"
+    if [ -z "$pipeline" ]; then
+        echo "usage: $0 <pipeline | all>"
         exit 1
     fi
 }
 
-function loggregator_set_pipeline {
-    local pipeline_name="$1"
+function set_pipeline {
+    local pipeline_name="loggregator"
     echo setting pipeline for "$pipeline_name"
     fly -t loggregator set-pipeline -p "$pipeline_name" \
-        -c "pipelines/loggregator/$pipeline_name.yml" \
+        -c "pipelines/$pipeline_name.yml" \
         -l ~/workspace/deployments-loggregator/shared-secrets.yml \
         -l ~/workspace/loggregator-ci/scripts.yml
 }
 
-function lakitu_set_pipeline {
-    local pipeline_name="$1"
-    echo setting pipeline for "$pipeline_name"
-    fly -t lakitu set-pipeline -p "$pipeline_name" \
-        -c "pipelines/lakitu/$pipeline_name.yml" \
-        -l ~/workspace/secrets-prod/ci/ci_bosh_secrets.yml \
-        -l ~/workspace/secrets-prod/ci/ci_github_resources.yml
-}
-
 function sync_fly {
-    fly -t "$env" sync
+    fly -t "$pipeline" sync
 }
 
 function set_pipelines {
     if [ "$pipeline" = all ]; then
-        for pipeline_file in $(ls "pipelines/$env"); do
+        for pipeline_file in $(ls "pipelines/"); do
             "${env}_set_pipeline" "${pipeline_file%.yml}"
         done
         exit 0
     fi
 
-    "${env}_set_pipeline" "$pipeline"
+    set_pipeline "$pipeline"
 }
 
 function main {
-    set_globals $1 $2
+    set_globals $1
     validate
     sync_fly
     set_pipelines
