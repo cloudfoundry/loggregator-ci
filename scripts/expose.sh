a661573fbe2faebad4f022a34a8c2dee3c7eff01
Eno Compton <ecompton@pivotal.io>
2017-12-07 13:43:12 -0700

    Update expose script

--- scripts/expose.sh
+++ scripts/expose.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 fly --target=loggregator expose-pipeline --pipeline=loggregator
-fly --target=loggregator expose-pipeline --pipeline=go-loggregator
-fly --target=loggregator expose-pipeline --pipeline=statsd-injector
-fly --target=loggregator expose-pipeline --pipeline=cf-syslog-drain
+fly --target=loggregator expose-pipeline --pipeline=go-packages
 fly --target=loggregator expose-pipeline --pipeline=bosh-hm-forwarder
-fly --target=loggregator expose-pipeline --pipeline=submodules
