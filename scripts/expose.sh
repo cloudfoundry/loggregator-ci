6f5ae3c6507eaead8fbc8938cd3eacfeb6012c6a
Brady Love <blove@pivotal.io>
2018-01-24 10:32:44 -0700

    Expose regression pipeline
    [#153706478]

--- scripts/expose.sh
+++ scripts/expose.sh
@@ -3,3 +3,4 @@
 fly --target=loggregator expose-pipeline --pipeline=loggregator
 fly --target=loggregator expose-pipeline --pipeline=go-packages
 fly --target=loggregator expose-pipeline --pipeline=bosh-hm-forwarder
+fly --target=loggregator expose-pipeline --pipeline=regression
