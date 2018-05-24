9ea665bb07c8a63b1cb635b171ae8387e0d2408e
Warren Fernandes <wfernandes@pivotal.io>
2018-05-24 09:51:59 -0600

    Add source_id filter for k8s pods to determine their source_id
    [#157690935]
    
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- docker-images/fluentd/plugins/loggregator.rb
+++ docker-images/fluentd/plugins/loggregator.rb
@@ -1,4 +1,5 @@
 require 'fluent/output'
+require 'fluent/filter'
 
 require 'ingress_services_pb'
 require 'envelope_pb'
@@ -36,12 +37,184 @@ module Fluent
 
         env.log = log
         env.timestamp = (Time.now.to_f * (10 ** 9)).to_i
-        env.source_id = record.fetch("kubernetes", {}).fetch("pod_name", "")
+        env.source_id = record.fetch("kubernetes", {}).fetch("owner", "")
         env.instance_id = record.fetch("kubernetes", {}).fetch("pod_id", "")
         batch.batch << env
 
-        @stub.send(batch)
+        begin
+          retries ||= 0
+          @stub.send(batch)
+        rescue GRPC::Unavailable => e
+          if (retries += 1) < 3
+            sleep 2
+            retry
+          else
+            raise e
+          end
+        end
       }
     end
+  end
+end
+
+module Fluent
+  class SourceIDFilter < Fluent::Filter
+    Fluent::Plugin.register_filter('source_id', self)
+
+    def configure(conf)
+      @client = KubernetesClient.new()
+    end
+
+    def filter(tag, time, record)
+      k8s = record.fetch("kubernetes")
+      if not k8s
+        return record
+      end
+
+      owner = resolve_owner(
+        k8s.fetch("namespace_name", ""),
+        "Pod",
+        k8s.fetch("pod_name", ""),
+      )
+      k8s["owner"] = owner
+
+      record
+    end
+
+    def resolve_owner(namespace_name, resource_type, resource_name)
+      id_fmt = "%s:%s:%s"
+
+      case resource_type
+      when "Pod"
+        obj = @client.get_pod(resource_name, namespace_name)
+      when "ReplicationController"
+        obj = @client.get_replicationcontroller(resource_name, namespace_name)
+      when "ReplicaSet"
+        obj = @client.get_replicaset(resource_name, namespace_name)
+      when "Deployment"
+        obj = @client.get_deployment(resource_name, namespace_name)
+      when "DaemonSet"
+        obj = @client.get_daemonset(resource_name, namespace_name)
+      when "StatefulSet"
+        obj = @client.get_statefulset(resource_name, namespace_name)
+      when "Job"
+        obj = @client.get_job(resource_name, namespace_name)
+      when "CronJob"
+        obj = @client.get_cronjob(resource_name, namespace_name)
+      else
+        obj = nil
+      end
+
+      if obj == nil
+        return id_fmt % [
+          namespace_name,
+          resource_type.downcase,
+          resource_name,
+        ]
+      end
+
+      ownerReferences = obj.fetch("metadata", {}).fetch("ownerReferences", [])
+      if ownerReferences.length == 0
+        return id_fmt % [
+          namespace_name,
+          resource_type.downcase,
+          resource_name,
+        ]
+      end
+
+      resolve_owner(
+        namespace_name,
+        ownerReferences[0]["kind"],
+        ownerReferences[0]["name"],
+      )
+    end
+  end
+end
+
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
+class KubernetesClient
+  def initialize
+    @url = "https://kubernetes.default.svc.cluster.local"
+    token_file = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+    ca_file = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+    @token = File.read(token_file)
+
+    uri = URI.parse(@url)
+    @http = Net::HTTP.new(uri.host, uri.port)
+    @http.use_ssl = true
+    @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    @http.ca_file = ca_file
+  end
+
+  def resource_url(namespace_name, resource_type, resource_name)
+    {
+      Pod: "%s/api/v1/namespaces/%s/pods/%s",
+      ReplicationController: "%s/api/v1/namespaces/%s/replicationcontrollers/%s",
+      ReplicaSet: "%s/apis/apps/v1/namespaces/%s/replicasets/%s",
+      Deployment: "%s/apis/apps/v1/namespaces/%s/deployments/%s",
+      DaemonSet: "%s/apis/apps/v1/namespaces/%s/daemonsets/%s",
+      StatefulSet: "%s/apis/apps/v1/namespaces/%s/statefulsets/%s",
+      Job: "%s/apis/batch/v1/namespaces/%s/jobs/%s",
+      CronJob: "%s/apis/batch/v1beta1/namespaces/%s/cronjobs/%s",
+    }[resource_type] % [@url, namespace_name, resource_name]
+  end
+
+  def make_request(namespace_name, resource_type, resource_name)
+    uri = URI.parse(resource_url(namespace_name, resource_type, resource_name))
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer " + @token
+    request
+  end
+
+  def get_pod(resource_name, namespace_name)
+    request = make_request(namespace_name, :Pod, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_replicationcontroller(resource_name, namespace_name)
+    request = make_request(namespace_name, :ReplicationController, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_replicaset(resource_name, namespace_name)
+    request = make_request(namespace_name, :ReplicaSet, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_deployment(resource_name, namespace_name)
+    request = make_request(namespace_name, :Deployment, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_daemonset(resource_name, namespace_name)
+    request = make_request(namespace_name, :DaemonSet, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_statefulset(resource_name, namespace_name)
+    request = make_request(namespace_name, :StatefulSet, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_job(resource_name, namespace_name)
+    request = make_request(namespace_name, :Job, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def get_cronjob(resource_name, namespace_name)
+    request = make_request(namespace_name, :CronJob, resource_name)
+    response = @http.request(request)
+    JSON.parse(response.body)
   end
 end
