a98713120e5c24f4a5497483b0f7ba9f062ea544
Johanna Smith <josmith@pivotal.io>
2018-06-28 17:26:56 -0600

    Add script for flying backport pipelines
    using their ops files
    
    [#157257263]

---
+++ scripts/set-backport-pipeline.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function set_globals {
+    product=$1
+    backport="$1-$2"
+    TARGET="${TARGET:-loggregator}"
+}
+
+function validate {
+    if [ "$product" = "-h" ] || [ "$product" = "--help" ] || [ -z "$product" ]; then
+        print_usage
+        exit 1
+    fi
+}
+
+function set_pipeline {
+    echo setting pipeline for $backport
+    fly -t $TARGET set-pipeline -p $backport \
+        -c <(bosh int "pipelines/$product.yml" -o "pipelines/backport-ops/$backport.yml")\
+        -l ~/workspace/deployments-loggregator/shared-secrets.yml \
+        -l ~/workspace/loggregator-ci/scripts.yml
+}
+
+function sync_fly {
+    fly -t $TARGET sync
+}
+
+function print_usage {
+    echo "usage: $0 <product> <version>"
+}
+
+function main {
+    set_globals $1 $2
+    validate
+    sync_fly
+    set_pipeline
+}
+main $1 $2
