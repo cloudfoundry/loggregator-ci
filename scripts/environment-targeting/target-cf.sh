0cc9914a141ce4c33bbc28b4882de99bdc7c135f
Max Eshleman <meshleman@pivotal.io>
2019-07-08 08:29:20 -0600

    target cf now attempts to create org and space before targeting

--- scripts/environment-targeting/target-cf.sh
+++ scripts/environment-targeting/target-cf.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 function credhub-get() (
-    set -e
-    var_name=$1
-    key_name=${2:-""}
+  set -e
+  var_name=$1
+  key_name=${2:-""}
 
-    pushd "bbl-state/${BBL_STATE_DIR}" > /dev/null
-        eval "$(bbl print-env)"
-    popd > /dev/null
+  pushd "bbl-state/${BBL_STATE_DIR}" > /dev/null
+    eval "$(bbl print-env)"
+  popd > /dev/null
 
-    key=""
-    if [[ -n "${key_name}" ]]; then
-        key=".${key_name}"
-    fi
-    credhub_find_reulsts=$(credhub find -j -n ${var_name})
-    echo ${credhub_find_reulsts} | jq -r .credentials[].name | xargs credhub get -j -n | jq -r ".value${key}"
+  key=""
+  if [[ -n "${key_name}" ]]; then
+    key=".${key_name}"
+  fi
+  credhub_find_reulsts=$(credhub find -j -n ${var_name})
+  echo ${credhub_find_reulsts} | jq -r .credentials[].name | xargs credhub get -j -n | jq -r ".value${key}"
 )
 
 function cf-password-from-credhub() (
@@ -21,27 +21,35 @@ function cf-password-from-credhub() (
     credhub-get cf_admin_password
 )
 
+function create-org-space-and-target() {
+  cf create-org "$ORG"
+  cf target -o "$ORG"
+
+  cf create-space "$SPACE"
+  cf target -s "$SPACE"
+}
+
 function target-cf() (
-    set -e
+  set -e
 
-    cf_flags=""
-    if [ "$SKIP_CERT_VERIFY" == "true" ]; then
-        cf_flags="${cf_flags} --skip-ssl-validation"
-    fi
+  cf_flags=""
+  if [[ "$SKIP_CERT_VERIFY" == "true" ]]; then
+    cf_flags="${cf_flags} --skip-ssl-validation"
+  fi
 
-    cf api "$CF_API" ${cf_flags}
+  cf api "$CF_API" ${cf_flags}
 
-    if [[ -n ${USE_CLIENT_AUTH} && "$USE_CLIENT_AUTH" == "true" ]]; then
-        cf_flags="${cf_flags} --client-credentials"
-    fi
+  if [[ -n ${USE_CLIENT_AUTH} && "$USE_CLIENT_AUTH" == "true" ]]; then
+    cf_flags="${cf_flags} --client-credentials"
+  fi
 
-    if [[ -d "bbl-state" ]]; then
-        PASSWORD=$(cf-password-from-credhub)
-    fi
+  if [[ -d "bbl-state" ]]; then
+    PASSWORD=$(cf-password-from-credhub)
+  fi
 
-    cf auth "$USERNAME" "$PASSWORD" ${cf_flags}
+  cf auth "$USERNAME" "$PASSWORD" ${cf_flags}
 
-    if [[ -n ${ORG} && -n ${SPACE} ]]; then
-        cf target -o "$ORG" -s "$SPACE"
-    fi
+  if [[ -n ${ORG} && -n ${SPACE} ]]; then
+    create-org-space-and-target
+  fi
 )
